156e05aa2c443f9b7f3bcd1dd73990949daccf6a
ibaguio <baguio.ivan@gmail.com>
2021-09-11 02:22:40 +0800

    add install.sh

---
+++ install.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+set -e
+
+if [[ -d ~/.git-helpers ]]; then
+	echo "Updating git-helpers..."
+	rm -fr ~/.git-helpers
+else
+	echo "Installing git-helpers..."
+fi
+
+# download the code
+git clone --quiet https://github.com/ibaguio/git-helpers.git ~/.git-helpers
+
+# make sure all helpers are executables
+chmod a+x ~/.git-helpers/bin/*
+
+# add git-helpers/bin to PATH
+cd ~/.git-helpers/bin/
+
+# install to bash
+if [[ -f ~/.bashrc ]]; then 
+	grep -qxF "export PATH=\$PATH:`pwd`" ~/.bashrc || cat <<EOT >> ~/.bashrc 
+
+# git-helpers
+alias g=git
+export PATH=\$PATH:`pwd`
+EOT
+fi
+
+# install to zsh
+if [[ -f ~/.zshrc ]]; then 
+	grep -qxF "export PATH=\$PATH:`pwd`" ~/.zshrc || cat <<EOT >> ~/.zshrc 
+
+# git-helpers
+alias g=git
+export PATH=\$PATH:`pwd`
+EOT
+fi
+
+# include custom aliases to gitconfig if it doesnt exist yet
+cat ~/.gitconfig | grep -q "path = ~/.git-helpers/alias.gitconfig" || {
+cat <<EOT >> ~/.gitconfig 
+
+[include]
+   # load custom aliases from git-helpers
+   path = ~/.git-helpers/alias.gitconfig
+EOT
+}
+
+echo "Success!!"
